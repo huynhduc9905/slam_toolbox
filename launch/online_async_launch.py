ff35ec380008b5c25885d032a4f7013dd59ca866
Chanh <chanhhoang99@gmail.com>
2024-12-31 16:36:36 +0700

    Add addition node to slam launch

--- launch/online_async_launch.py
+++ launch/online_async_launch.py
@@ -12,7 +12,9 @@ from launch_ros.actions import LifecycleNode
 from launch_ros.event_handlers import OnStateTransition
 from launch_ros.events.lifecycle import ChangeState
 from lifecycle_msgs.msg import Transition
-
+from launch.actions import ExecuteProcess
+from launch.actions import IncludeLaunchDescription
+from launch.launch_description_sources import PythonLaunchDescriptionSource
 
 def generate_launch_description():
     autostart = LaunchConfiguration('autostart')
@@ -37,6 +39,9 @@ def generate_launch_description():
                                    'config', 'mapper_params_online_async.yaml'),
         description='Full path to the ROS2 parameters file to use for the slam_toolbox node')
 
+    map_saver_dir = get_package_share_directory('nav2_map_server')
+    map_saver_launch_dir = os.path.join(map_saver_dir, 'launch')
+
     start_async_slam_toolbox_node = LifecycleNode(
         parameters=[
           slam_params_file,
@@ -49,7 +54,38 @@ def generate_launch_description():
         executable='async_slam_toolbox_node',
         name='slam_toolbox',
         output='screen',
+        namespace='',
+        remappings=[
+            ('/map', '/map_navi'),  # Remap service
+    ]
+    )
+
+    start_map_to_img_node = LifecycleNode(
+        package='map_to_img',
+        executable='map_to_img',
+        name='map_to_img',
+        output='screen',
         namespace=''
+    )
+
+    start_pose_publisher_node = LifecycleNode(
+        package='pose_publisher',
+        executable='pose_publisher',
+        name='pose_publisher',
+        output='screen',
+        namespace=''
+    )
+
+    start_map_mask_node = LifecycleNode(
+        package='map_mask',
+        executable='map_mask',
+        name='map_mask',
+        output='screen',
+        namespace=''
+    )
+
+    start_map_saver = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(os.path.join(map_saver_launch_dir, 'map_saver_server.launch.py')),
     )
 
     configure_event = EmitEvent(
@@ -76,13 +112,25 @@ def generate_launch_description():
         condition=IfCondition(AndSubstitution(autostart, NotSubstitution(use_lifecycle_manager)))
     )
 
+    enable_odrive_tf = ExecuteProcess(
+        cmd=[
+            'ros2', 'param', 'set', '/odrive_python', 'pub_odrive_tf', 'true'
+        ],
+        output='screen'
+    )
+
     ld = LaunchDescription()
 
     ld.add_action(declare_autostart_cmd)
+    ld.add_action(enable_odrive_tf)
     ld.add_action(declare_use_lifecycle_manager)
     ld.add_action(declare_use_sim_time_argument)
     ld.add_action(declare_slam_params_file_cmd)
     ld.add_action(start_async_slam_toolbox_node)
+    ld.add_action(start_map_saver)
+    ld.add_action(start_map_to_img_node)
+    ld.add_action(start_pose_publisher_node)
+    ld.add_action(start_map_mask_node)
     ld.add_action(configure_event)
     ld.add_action(activate_event)
 
